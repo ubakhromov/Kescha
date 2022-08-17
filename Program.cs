00fad9d822a4bb4a54a1e1ed0493bc2325e09dac
tatubaqqol@gmail.com <tatubaqqol@gmail.com>
2022-08-17 23:34:39 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please, type your first and second number");
+Console.Write("1st number =  ");
+int firstNumber = int.Parse(Console.ReadLine());
+System.Console.Write("2nd number =  ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"addition is equal to: {firstNumber + secondNumber}");
+Console.WriteLine($"subtraction is equal to: {firstNumber - secondNumber}");
+Console.WriteLine($"multiplication is equal to: {firstNumber * secondNumber}");
+Console.WriteLine($"division is equal to: {firstNumber / secondNumber}");
