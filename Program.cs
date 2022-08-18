c095db26d8fda6ce1c75ef0efb4974a6462eb8e2
tatubaqqol@gmail.com <tatubaqqol@gmail.com>
2022-08-18 13:05:32 +0500

    Teach if Conditions

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ System.Console.Write($"\n{name}, enter your age, please: ");
 int age = int.Parse(Console.ReadLine());
 int keschasAge = 3;
 
-
-System.Console.WriteLine($"{name} your age is older than Kescha's age: {age > keschasAge}");
-
-System.Console.WriteLine($"{name} your age is younger than Kescha's age: {age < keschasAge}");
-
-System.Console.WriteLine($"{name} your age is equal to Kescha's age: {age == keschasAge}");
+if(age < keschasAge)
+{
+    System.Console.WriteLine("You are younger than Kescha");
+}else if(age==keschasAge)
+{
+    System.Console.WriteLine("you are equals");
+}else
+{
+    System.Console.WriteLine("You are older");
+}
